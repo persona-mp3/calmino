8a8b09e2dfb5e4457e9835fedd8692cc5d7c533b
persona-mp3 <randomnobscurebs@gmail.com>
2026-08-31 10:50:30 +0100

    feat: build abstraction over rpc conn

--- node.go
+++ node.go
@@ -1,13 +1,24 @@
 package main
 
-import "sync"
+import (
+	"log/slog"
+	"sync"
+)
 
 type Node struct {
-	mu   sync.Mutex
-	id   string
-	term uint64
+	mu sync.Mutex
+	// id hold the current id of the node in the cluster
+	id string
+
+	// peers holds the remote addresses of other peers in the cluster
+	peers []string
+
+	// connections hold rpc connections to different nodes
+	connections []*RPCConn
 
 	server    Server
 	raftState RaftState
 	logStore  LogStore
+
+	logger *slog.Logger
 }
---
+++ peer.go
@@ -0,0 +1,39 @@
+package main
+
+import "net/rpc"
+
+// RPCConn provides an abstraction over rpc.Client
+type RPCConn interface {
+	Id() string
+	Addr() string
+	Close() error
+	Call(serviceName string, req any, reply any) error
+}
+
+type RPCPeer struct {
+	// id holds the current id of the connection
+	id string
+	// addr holds the address this peer is connected to
+	addr string
+	// conn is the underlying *rpc.Client connection
+	conn *rpc.Client
+}
+
+func (rp RPCPeer) Id() string {
+	return rp.id
+}
+
+func (rp RPCPeer) Addr() string {
+	return rp.addr
+}
+
+func (rp RPCPeer) Call(serviceName string, req any, reply any) error {
+	return rp.conn.Call(serviceName, req, reply)
+}
+
+func (rp RPCPeer) Close() error {
+	return rp.conn.Close()
+}
+func NewRPCPeer(id, addr string, conn *rpc.Client) RPCConn {
+	return RPCPeer{id: id, addr: addr, conn: conn}
+}
