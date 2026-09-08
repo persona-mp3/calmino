49b42dc05509dcf744d70163af7ab2dec91dde1a
persona-mp3 <randomnobscurebs@gmail.com>
2026-09-08 14:44:52 +0100

    design: suggested single loop way

---
+++ other.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// What if we just had a single loop sending out the heartbeats? instead of
+// seperate workers?
+
+func wokerLoop(
+	ctx context.Context,
+	id NodeId,
+	replicateCh chan replicate,
+	peers []RPCConn,
+	store LogStore,
+	term uint64,
+	raft *RaftState,
+) {
+	ticker := time.NewTicker(time.Millisecond * 300)
+	defer func() {
+		ticker.Stop()
+		raft.UpdateState(StateFollower)
+	}()
+
+	// exit is closed  by sendHB when a majority of the cluster has ignored this node
+	exit := make(chan struct{})
+
+	prevLogEntry := store.PreviousEntry()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case replica := <-replicateCh:
+			for _, peer := range peers {
+				go func(success chan struct{}) {
+					// TODO: need a timeout here incase the client is blocking to avoid leaks
+					reply := AppendEntryReply{}
+					if err := peer.Call("Server.AppendEntry", replica.req, &reply); err != nil {
+						log.Println(err)
+					}
+					switch reply.Result {
+					case RaftResultAcked, RaftResultLogsOutOfSync:
+						select {
+						case success <- struct{}{}:
+						default:
+							log.Println("dropping result from replication, receiver blocked")
+						}
+					}
+				}(replica.replicated)
+			}
+
+		case <-ticker.C:
+			req := AppendEntryRequest{
+				Id:               id,
+				Term:             term,
+				PreviousLogIndex: prevLogEntry.Index,
+				PreviousLogTerm:  prevLogEntry.Term,
+			}
+			// TODO: might need a goroutine pool here?
+			go func() {
+				err := sendHB(ctx, peers, req, exit)
+				if err != nil {
+					log.Println("[error] from sendHB", err)
+				}
+			}()
+			ticker.Reset(time.Millisecond * 300)
+		case <-exit:
+			log.Println("[info] exit channel fired")
+			return
+		}
+	}
+}
+
+func sendHB(ctx context.Context, peers []RPCConn, req AppendEntryRequest, exit chan struct{}) error {
+	failed := atomic.Uint64{}
+	wg := sync.WaitGroup{}
+	for _, peer := range peers {
+		wg.Add(1)
+		go func(peer RPCConn, failed *atomic.Uint64) {
+			reply := AppendEntryReply{}
+			if err := peer.Call("Server.AppendEntry", req, &reply); err != nil {
+				log.Println("[error] sending heartbeat", err)
+				failed.Add(1)
+				return
+			}
+
+			switch reply.Result {
+			case RaftResultAcked, RaftResultLogsOutOfSync:
+			default:
+				log.Printf("[info] was not acked by follower: %+v\n", reply)
+				failed.Add(1)
+			}
+		}(peer, &failed)
+	}
+
+	done := make(chan struct{}, 1)
+	go func() {
+		wg.Wait()
+		close(done)
+		log.Println("all background routines returned")
+	}()
+
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("from sendHB: %w", ctx.Err())
+	case <-done:
+	}
+	ackedByMajority := failed.Load() <= uint64(len(peers))
+	if !ackedByMajority {
+		close(exit)
+	}
+	return nil
+}
--- worker.go
+++ worker.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"context"
-	"log/slog"
-	"sync/atomic"
-	"time"
-)
-
 type replicate struct {
 	// replicated is used to signify an entry that have been successfully replicated
 	// on a peer
@@ -15,68 +8,94 @@ type replicate struct {
 	req AppendEntryRequest
 }
 
-type Worker struct {
-	id        string
-	commitIdx *atomic.Uint64
-	// replicateCh is used by the leader to start a repication across peers
-	replicateCh chan replicate
-}
-
-var SendChanTimeout = 300 * time.Millisecond
-
-// startReplication starts a replication across the cluster via the workers.
+// type Worker struct {
+// 	id        NodeId
+// 	commitIdx *atomic.Uint64
+// 	// replicateCh is used by the leader to start a repication across peers
+// 	replicateCh chan replicate
+// }
 //
-// If a quorum is reached ie a majority of the cluster have replicated the request
-// to their logs, it returns true, otherwise false. The [dur] is used to ensure
-// that replication has a hard set limit
-func startReplication(
-	ctx context.Context,
-	req AppendEntryRequest,
-	dur time.Duration,
-	clusterSize int,
-	workers []*Worker,
-	logger *slog.Logger,
-) bool {
-	// make sure workers can still send if no receiver
-	replicated := make(chan struct{}, len(workers)*2)
-
-	entry := replicate{
-		replicated: replicated,
-		req:        req,
-	}
-
-	for _, worker := range workers {
-		go func(w *Worker) {
-			ticker := time.NewTicker(SendChanTimeout)
-			defer ticker.Stop()
-			select {
-			case worker.replicateCh <- entry:
-			case <-ticker.C:
-				logger.Warn("could not send replication entry to worker", "workerId", worker.id)
-				return
-			}
-		}(worker)
-	}
-
-	// this node has already made a copy to its logs
-	replicasMade := 1
-	replicationTimeoutCtx, cancel := context.WithTimeout(ctx, dur)
-	quorum := (clusterSize / 2) + 1
-	defer cancel()
-	for {
-		if replicasMade >= quorum {
-			return true
-		} else if err := replicationTimeoutCtx.Err(); err != nil {
-			logger.Info("replication timeout reached")
-			break
-		}
-
-		select {
-		case <-replicated:
-			replicasMade += 1
-		case <-replicationTimeoutCtx.Done():
-		}
-	}
-
-	return replicasMade >= quorum
-}
+// var SendChanTimeout = 300 * time.Millisecond
+//
+// func NewWorker(
+// 	id NodeId,
+// 	commitIdx *atomic.Uint64,
+// 	replicateCh chan replicate,
+// ) *Worker {
+// 	return &Worker{id: id, commitIdx: commitIdx, replicateCh: replicateCh}
+// }
+//
+// func (w *Worker) Run(ctx context.Context, term uint64, peer RPCConn) {
+// 	ticker := time.NewTicker(time.Duration(HEARTBEAT_INTERVAL) * time.Millisecond)
+// 	defer ticker.Stop()
+// 	req := AppendEntryRequest{
+// 		Id:   w.id,
+// 		Term: term,
+// 	}
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			return
+// 		case <-ticker.C:
+// 			reply := AppendEntryReply{}
+// 			peer.Call("Server.AppendEntry", req, &reply)
+// 		}
+// 	}
+// }
+//
+// // startReplication starts a replication across the cluster via the workers.
+// //
+// // If a quorum is reached ie a majority of the cluster have replicated the request
+// // to their logs, it returns true, otherwise false. The [dur] is used to ensure
+// // that replication has a hard set limit
+// func startReplication(
+// 	ctx context.Context,
+// 	req AppendEntryRequest,
+// 	dur time.Duration,
+// 	clusterSize int,
+// 	workers []*Worker,
+// 	logger *slog.Logger,
+// ) bool {
+// 	// make sure workers can still send if no receiver
+// 	replicated := make(chan struct{}, len(workers)*2)
+//
+// 	entry := replicate{
+// 		replicated: replicated,
+// 		req:        req,
+// 	}
+//
+// 	for _, worker := range workers {
+// 		go func(w *Worker) {
+// 			ticker := time.NewTicker(SendChanTimeout)
+// 			defer ticker.Stop()
+// 			select {
+// 			case worker.replicateCh <- entry:
+// 			case <-ticker.C:
+// 				logger.Warn("could not send replication entry to worker", "workerId", worker.id)
+// 				return
+// 			}
+// 		}(worker)
+// 	}
+//
+// 	// this node has already made a copy to its logs
+// 	replicasMade := 1
+// 	replicationTimeoutCtx, cancel := context.WithTimeout(ctx, dur)
+// 	quorum := (clusterSize / 2) + 1
+// 	defer cancel()
+// 	for {
+// 		if replicasMade >= quorum {
+// 			return true
+// 		} else if err := replicationTimeoutCtx.Err(); err != nil {
+// 			logger.Info("replication timeout reached")
+// 			break
+// 		}
+//
+// 		select {
+// 		case <-replicated:
+// 			replicasMade += 1
+// 		case <-replicationTimeoutCtx.Done():
+// 		}
+// 	}
+//
+// 	return replicasMade >= quorum
+// }
